f8f38bfb494f027157547f6fe1b00997e080a894
Jeremiah Coleman <32949393+jeremiahdc23@users.noreply.github.com>
2022-09-28 13:01:37 -0400

    Added inductons

--- scqubits/__init__.py
+++ scqubits/__init__.py
@@ -36,6 +36,7 @@ from scqubits.core.oscillator import KerrOscillator, Oscillator
 from scqubits.core.param_sweep import ParameterSweep
 from scqubits.core.storage import DataStore, SpectrumData
 from scqubits.core.transmon import Transmon, TunableTransmon
+from scqubits.core.inducton import Inducton, TunableInducton
 from scqubits.core.units import (
     from_standard_units,
     get_units,
